24f23545a8bf6b90e391ce53604695f29c51821e
Akanksha Gangrade <akankshagangrade27@gmail.com>
2026-01-28 23:00:32 +0530

    Setup

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# End to end Machine Learning projecr
+# End to end Machine Learning project
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT="-e ."
+
+def get_requirements(file_path:str)->list[str]:
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="MLProject",
+    version="0.0.1",
+    author="Akanksha",
+    author_email="Akankshagangrade27@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
